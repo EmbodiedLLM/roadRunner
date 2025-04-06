1d44bb5426bdbf19f84eb69ef10d24439ab8386e
acyanbird <acyanbird@gmail.com>
2025-04-06 21:18:39 +0800

    init

--- 2d_slam/build/lslidar_driver/colcon_command_prefix_build.sh
+++ 2d_slam/build/lslidar_driver/colcon_command_prefix_build.sh
@@ -1,2 +1,2 @@
 # generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/nvidia/2d_slam/install/lslidar_msgs/share/lslidar_msgs/package.sh"
+. "/home/nvidia/roadRunner/2d_slam/install/lslidar_msgs/share/lslidar_msgs/package.sh"
